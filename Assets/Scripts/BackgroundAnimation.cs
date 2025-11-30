f1e68be265a9fed1adba8ddfc441173f5bcb8fb3
Pocoloco115 <waltorres@uamv.edu.ni>
2025-11-30 13:14:35 -0600

    Mejoras en el menu principal

---
+++ Assets/Scripts/BackgroundAnimation.cs
@@ -0,0 +1,29 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundAnimation : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private RectTransform canvas;
+    private float imageWidth;
+    private float canvasWidth;
+    private RectTransform rt;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+        imageWidth = rt.rect.width;
+        canvasWidth = canvas.rect.width;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rt.anchoredPosition += Vector2.left * speed * Time.deltaTime;
+        if(rt.anchoredPosition.x <= -canvasWidth/2f - imageWidth)
+        {
+            rt.anchoredPosition = new Vector2(canvasWidth / 2f + imageWidth, rt.anchoredPosition.y);
+        }
+    }
+}
